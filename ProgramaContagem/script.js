566c83b99ad766cb1b9d19d19f58b6eeb8dc99cb
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2024-10-10 10:59:41 -0300

    Programa de contagem
    Aprendendo a criar um programa que permite aumentar, diminuir ou resetar o texto de contagem

---
+++ ProgramaContagem/script.js
@@ -0,0 +1,20 @@
+const decreaseButton = document.querySelector("#drecrease");
+const resetButton = document.querySelector("#reset");
+const increaseButton = document.querySelector("#increase");
+const numberCounter = document.querySelector(".number");
+let initialCount = 0
+let count = 0;
+
+decreaseButton.onclick = function() {
+    count--;
+    numberCounter.textContent = count;
+
+}
+
+resetButton.onclick = function() {
+    count = initialCount;
+    numberCounter.textContent = count;
+}
+
+
+
