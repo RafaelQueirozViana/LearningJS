ab16a997698bd8f05877784033df25da60e9cee1
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2025-03-21 14:38:56 -0300

    UpdateDado

--- JogoRolarDado/script.js
+++ JogoRolarDado/script.js
@@ -1,12 +1,63 @@
-const input = document.getElementById("inpt");
-const button = document.getElementById("btn");
 
-button.addEventListener("click", function () {
-    let inputValue = input.value;
-    for (let i = 0; i < inputValue; i++) {
-        const randomNum = Math.floor(Math.random() * 6) + 1;
-        console.log(randomNum)
+const rollButton = document.getElementById("roll-button");
+const sideButtons = document.querySelectorAll(".side-btn");
+const diceContainer = document.querySelector(".dice-container");
+const totalText = document.getElementById("total-txt");
+
+
+let totalDices = 2;
+let diceValues = [];
+
+
+
+class Dice {
+    constructor(sides) {
+        this.sides = sides;
+        this.currentNumber = 1;
+
+        this.img = document.createElement('img');
+        this.img.src = `./Imagens/${this.currentNumber}.jpg`;
+
+        diceContainer.appendChild(this.img);
     }
 
 
+
+    roll = () => {
+        this.currentNumber = Math.floor(Math.random() * this.sides) + 1;
+
+        diceValues.push(this.currentNumber);
+
+
+    }
+
+    showTotal = () => {
+        totalText.textContent = diceValues.reduce((summedNumber, currentNumber) => summedNumber + currentNumber, 0)
+
+    }
+
+
+}
+
+const dado1 = new Dice(10);
+dado1.roll();
+
+dado1.showTotal()
+
+
+const dado2 = new Dice(5);
+dado2.roll();
+dado2.showTotal()
+
+console.log(diceValues);
+
+
+rollButton.addEventListener("click", function () {
+
+
+    dado1.showTotal()
 })
+
+
+
+
