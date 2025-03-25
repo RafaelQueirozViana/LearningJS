8e1239c99a589160caf22ff9545cef52b9fc851c
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2025-03-25 09:45:45 -0300

    dadoFinalizado

--- JogoRolarDado/script.js
+++ JogoRolarDado/script.js
@@ -1,62 +1,140 @@
-
 const rollButton = document.getElementById("roll-button");
 const sideButtons = document.querySelectorAll(".side-btn");
 const diceContainer = document.querySelector(".dice-container");
 const totalText = document.getElementById("total-txt");
 
-
-let totalDices = 2;
-let diceValues = [];
-
-
+let dicesArray = [];
 
 class Dice {
     constructor(sides) {
         this.sides = sides;
-        this.currentNumber = 1;
-
+        this.currentNumber = Math.floor(Math.random() * (this.sides) + 1);
         this.img = document.createElement('img');
         this.img.src = `./Imagens/${this.currentNumber}.jpg`;
+        this.img.addEventListener("click", () => this.remove());
 
         diceContainer.appendChild(this.img);
+        dicesArray.push(this);
     }
 
-
-
-    roll = () => {
-        this.currentNumber = Math.floor(Math.random() * this.sides) + 1;
-
-        diceValues.push(this.currentNumber);
-
-
+    roll() {
+        this.currentNumber = Math.floor(Math.random() * (this.sides) + 1);
+        this.img.src = `./Imagens/${this.currentNumber}.jpg`;
     }
 
-    showTotal = () => {
-        totalText.textContent = diceValues.reduce((summedNumber, currentNumber) => summedNumber + currentNumber, 0)
-
+    remove() {
+        diceContainer.removeChild(this.img);
+        let index = dicesArray.indexOf((this));
+        dicesArray.splice(index, 1)
     }
-
 
 }
 
-const dado1 = new Dice(10);
-dado1.roll();
+const dado1 = new Dice(6);
 
-dado1.showTotal()
+const dado2 = new Dice(8);
 
 
-const dado2 = new Dice(5);
-dado2.roll();
-dado2.showTotal()
+function totalValue() {
+    const soma = dicesArray.reduce((total, current) => {
+        return total + current.currentNumber;
+    }, 0);
 
-console.log(diceValues);
+    totalText.textContent = soma
+
+}
+
+sideButtons.forEach(currentButton => {
+    currentButton.addEventListener("click", function () {
+        const sides = parseInt(currentButton.textContent);
+        const newDice = new Dice(sides);
+    })
+})
+
 
 
 rollButton.addEventListener("click", function () {
+    dicesArray.forEach(dice => {
+        dice.roll();
 
+    })
+    totalValue()
+    console.log(dicesArray);
 
-    dado1.showTotal()
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const rollButton = document.getElementById("roll-button");
+// const sideButtons = document.querySelectorAll(".side-btn");
+// const diceContainer = document.querySelector(".dice-container");
+// const totalText = document.getElementById("total-txt");
+
+
+// let totalDices = 2;
+// let diceValues = [];
+
+
+
+// class Dice {
+//     constructor(sides) {
+//         this.sides = sides;
+//         this.currentNumber = 1;
+
+//         this.img = document.createElement('img');
+//         this.img.src = `./Imagens/${this.currentNumber}.jpg`;
+
+//         diceContainer.appendChild(this.img);
+//     }
+
+
+
+//     roll = () => {
+//         this.currentNumber = Math.floor(Math.random() * this.sides) + 1;
+
+//         diceValues.push(this.currentNumber);
+
+
+//     }
+
+//     showTotal = () => {
+//         totalText.textContent = diceValues.reduce((summedNumber, currentNumber) => summedNumber + currentNumber, 0)
+
+//     }
+
+
+// }
+
+// const dado1 = new Dice(10);
+// dado1.roll();
+
+// dado1.showTotal()
+
+
+// const dado2 = new Dice(5);
+// dado2.roll();
+// dado2.showTotal()
+
+// console.log(diceValues);
+
+
+// rollButton.addEventListener("click", function () {
+
+
+//     dado1.showTotal()
+// })
 
 
 
