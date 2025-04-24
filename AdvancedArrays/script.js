21690e633063824e2778b9bfba72b9839ce1bc6a
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2025-04-24 15:51:30 -0300

    learning

--- AdvancedArrays/script.js
+++ AdvancedArrays/script.js
@@ -5,16 +5,22 @@
 // .push --> this will adds a value to the end of the array. Ex: array.push(100);
 // .slice --> this will remove a value of the array. Ex: array.slice(index, 10) -This will remove the values of the array length between index and 10
 
-const doArray = [1, 3, '4'];
+const doArray = [];
+const input = document.getElementById('inp');
+const button = document.getElementById('but');
+const displayCont = document.querySelector('.texts-container');
 
-let total = 0
 
-for (let i = 0; i <= doArray.length - 1; i++) {
-    total += Number(doArray[i]);
+const addTodo = () => {
+    doArray.push(input.value);
+    const p = document.createElement('p');
+    displayCont.appendChild(p);
+    p.textContent = input.value;
+
+
+
+
 }
 
-console.log(total)
-
-
-
+button.addEventListener('click', addTodo);
 
