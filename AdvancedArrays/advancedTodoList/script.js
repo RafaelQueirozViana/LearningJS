04a2a22303469d96382c721473fb5f29e0a8b8e2
RafaelQueirozViana <rafaelqueirozcomercial3@gmail.com>
2025-04-27 16:56:52 -0300

    adding delete button, localStorage and css for the Advanced To do Lisr

--- AdvancedArrays/advancedTodoList/script.js
+++ AdvancedArrays/advancedTodoList/script.js
@@ -1,25 +1,60 @@
 const addButton = document.getElementById('but');
-const listContainer = document.getElementById('do-container');
+const listContainer = document.getElementById('list-container');
 const doInput = document.getElementById('do');
 const dateInput = document.getElementById('date');
+const errorText = document.getElementById('error-text');
 
-let listArray = [];
+
+let listArray = JSON.parse(localStorage.getItem('doList')) || [];
 
 const addTodo = () => {
+
+    if (!doInput.value || !dateInput.value) {
+        showError('please, fill in all fields ')
+    }
+
+    else {
+        showError('')
+
+        listArray.push({
+            task: doInput.value,
+            date: dateInput.value,
+        });
+
+        renderList();
+    }
+
 
 };
 
 const renderList = () => {
+    localStorage.setItem('doList', JSON.stringify(listArray));
+
+    console.log(localStorage.getItem('doList'))
+
     listContainer.innerHTML = '';
-    listArray.push(d)
-
     listArray.forEach(current => {
-        const newText = document.createElement('p');
-        newText.textContent = current;
-        listContainer.appendChild(newText);
+        listContainer.innerHTML += `
+        <div class="do-container">
+               <p class="name">${current.task}</p>
+               <p class="date">${current.date}</p>
+               <button onclick="removeTask(event)" type="button">Delete</button>
+           </div>`
     });
-
 };
 
+const removeTask = (event) => {
+    const taskName = event.target.parentElement.querySelector('.name').textContent;
+    const index = listArray.findIndex(current => current.task === taskName);
+    listArray.splice(index, 1);
+    renderList();
+}
 
-addButton.addEventListener('click', renderList);
+const showError = (message) => {
+    errorText.textContent = message;
+}
+
+renderList();
+addButton.addEventListener('click', addTodo);
+
+
