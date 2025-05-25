af88a4912b4185c046e2d3684bbe1223b5ea44f3
Rafael <rafaelviana.contact@gmail.com>
2025-05-25 13:23:50 -0300

    Separating the data script and creating the html products generator

--- AmazonProject/scripts/amazon.js
+++ AmazonProject/scripts/amazon.js
@@ -1,35 +1,3 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87,
-  },
-  priceCents: 1090
-},
-
-{
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127,
-  },
-  priceCents: 1090
-},
-
-{
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4,
-    count: 56,
-  },
-  priceCents: 1090
-},
-
-
-];
 
 const productGrid = document.querySelector('.products-grid');
 
@@ -39,10 +7,10 @@ const loadProducts = () => {
   productGrid.innerHTML = '';
   products.forEach((product) => {
 
-    const html = `   <div class="product-container">
+    const html = ` <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
