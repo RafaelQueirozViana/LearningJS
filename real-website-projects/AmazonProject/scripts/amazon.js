c179e0135fd4f10a8b911600f89c25304de16bde
Rafael <rafaelviana.contact@gmail.com>
2025-05-31 14:47:51 -0300

    turning the load products function as a class

--- real-website-projects/AmazonProject/scripts/amazon.js
+++ real-website-projects/AmazonProject/scripts/amazon.js
@@ -85,7 +85,8 @@ class LoadProductsInPage {
     buttons.forEach((button, index) => {
       button.addEventListener('click', () => {
         addToCart(index)
-        showCartQuantity()
+        this.showCartQuantity();
+
 
       });
     });
@@ -98,54 +99,8 @@ class LoadProductsInPage {
 
 
 const amazonProducts = new LoadProductsInPage({
-  container: productGrid, htmlCode: ` <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
-
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
-
-          <div class="product-rating-container">
-            <img class="${product.image}"
-              src="images/ratings/rating-${product.rating.stars * 10}.png">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
-            </div>
-          </div>
-
-          <div class="product-price">
-           $ ${(product.priceCents) / 100}
-          </div>
-
-          <div class="product-quantity-container">
-            <select class="js-quantity-selector">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart js-add-message">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart">
-            Add to Cart
-          </button>
-      </div>` });
+  container: productGrid,
+});
 
 
 amazonProducts.displayInHtml()
