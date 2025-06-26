505d7e1a5d34ad2dc091b635fb5cd710effbafab
Rafael <rafaelviana.contact@gmail.com>
2025-06-26 13:56:03 -0300

    recreating ans restructuring

--- real-website-projects/AmazonProject/scripts/amazon.js
+++ real-website-projects/AmazonProject/scripts/amazon.js
@@ -1,19 +1,12 @@
-import { cart, addToCart } from '../data/cart.js'; // Data of all products in Cart  
-import { products } from '../data/products.js'; // Data of all Products in the website
-
+import { products } from '../data/products.js';
+import { cart } from '../data/cart.js';
 
 const productGrid = document.querySelector('.js-all-products');
-const cartText = document.querySelector('.js-cart-quantity');
 
-const showCartQuantity = () => {
-  const sumCartProducts = cart.reduce((acumulator, cartItem) => acumulator += cartItem.quantity, 0);
-  cartText.textContent = sumCartProducts;
-}
 
 const loadProducts = () => {
-  showCartQuantity()
   productGrid.innerHTML = '';
-  products.forEach((product) => {
+  products.forEach(product => {
 
     const html = ` <div class="product-container">
           <div class="product-image-container">
@@ -34,7 +27,7 @@ const loadProducts = () => {
           </div>
 
           <div class="product-price">
-           $ ${(product.priceCents) / 100}
+           $ ${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -67,28 +60,138 @@ const loadProducts = () => {
     productGrid.innerHTML += html;
   });
 
-  // Creating the EventListener in the all buttons that we are creating
 
-  const buttons = document.querySelectorAll('.js-add-to-cart');
+  //creating  addToCart function Event Listener to these products
 
-  buttons.forEach((button, index) => {
-    const addedCartMessage = document.querySelectorAll('.js-add-message')[index];
+  const addButtons = document.querySelectorAll('.js-add-to-cart');
 
+  addButtons.forEach((button, position) => {
     button.addEventListener('click', () => {
-      addToCart(index)
-      showCartQuantity()
-
-      addedCartMessage.style.opacity = 1;
-
-      setTimeout(() => {
-        addedCartMessage.style.opacity = 0;
-
-      }, 2000)
-
+      console.log('click')
+      cart.push(products[position]);
+      console.log(cart)
     });
   });
 
+
+
+
+
+
+
+
+
 };
 
-loadProducts()
+
+
+
+loadProducts();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { cart, addToCart } from '../data/cart.js'; // Data of all products in Cart
+// import { products } from '../data/products.js'; // Data of all Products in the website
+
+
+// const productGrid = document.querySelector('.js-all-products');
+// const cartText = document.querySelector('.js-cart-quantity');
+
+// const showCartQuantity = () => {
+//   const sumCartProducts = cart.reduce((acumulator, cartItem) => acumulator += cartItem.quantity, 0);
+//   cartText.textContent = sumCartProducts;
+// }
+
+// const loadProducts = () => {
+//   showCartQuantity()
+//   productGrid.innerHTML = '';
+//   products.forEach((product) => {
+
+//     const html = ` <div class="product-container">
+//           <div class="product-image-container">
+//             <img class="product-image"
+//               src="${product.image}">
+//           </div>
+
+//           <div class="product-name limit-text-to-2-lines">
+//             ${product.name}
+//           </div>
+
+//           <div class="product-rating-container">
+//             <img class="${product.image}"
+//               src="images/ratings/rating-${product.rating.stars * 10}.png">
+//             <div class="product-rating-count link-primary">
+//               ${product.rating.count}
+//             </div>
+//           </div>
+
+//           <div class="product-price">
+//            $ ${(product.priceCents / 100).toFixed(2)}
+//           </div>
+
+//           <div class="product-quantity-container">
+//             <select class="js-quantity-selector">
+//               <option selected value="1">1</option>
+//               <option value="2">2</option>
+//               <option value="3">3</option>
+//               <option value="4">4</option>
+//               <option value="5">5</option>
+//               <option value="6">6</option>
+//               <option value="7">7</option>
+//               <option value="8">8</option>
+//               <option value="9">9</option>
+//               <option value="10">10</option>
+//             </select>
+//           </div>
+
+//           <div class="product-spacer"></div>
+
+//           <div class="added-to-cart js-add-message">
+//             <img src="images/icons/checkmark.png">
+//             Added
+//           </div>
+
+//           <button class="add-to-cart-button button-primary js-add-to-cart">
+//             Add to Cart
+//           </button>
+//         </div>`;
+
+//     productGrid.innerHTML += html;
+//   });
+
+//   // Creating the EventListener in the all buttons that we are creating
+
+//   const buttons = document.querySelectorAll('.js-add-to-cart');
+
+//   buttons.forEach((button, index) => {
+//     const addedCartMessage = document.querySelectorAll('.js-add-message')[index];
+
+//     button.addEventListener('click', () => {
+//       addToCart(index)
+//       showCartQuantity()
+
+//       addedCartMessage.style.opacity = 1;
+
+//       setTimeout(() => {
+//         addedCartMessage.style.opacity = 0;
+
+//       }, 2000)
+
+//     });
+//   });
+
+// };
+
+// loadProducts()
 
