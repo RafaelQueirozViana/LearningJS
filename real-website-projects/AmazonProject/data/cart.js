a8aecad2d62bd78df91d96763b62ee368fc963ea
Rafael <rafaelviana.contact@gmail.com>
2025-07-16 15:38:43 -0300

    cleaned the cart data script

--- real-website-projects/AmazonProject/data/cart.js
+++ real-website-projects/AmazonProject/data/cart.js
@@ -113,34 +113,3 @@ export const resetCart = () => {
 
 
 
-
-// export const addToCart = (productIndex) => {
-
-//     const currentProduct = products[productIndex];
-//     const selectQuantityBtn = document.querySelectorAll('.js-quantity-selector');
-//     const Selectedquantity = Number(selectQuantityBtn[productIndex].value);
-
-
-//     let matchingItem = cart.find(cartItem => cartItem.id === currentProduct.id);
-
-//     if (!matchingItem) {
-//         cart.push({
-//             name: currentProduct.name,
-//             price: currentProduct.priceCents,
-//             image: currentProduct.image,
-//             quantity: Selectedquantity,
-//             id: currentProduct.id
-//         });
-//     }
-
-//     else {
-//         matchingItem.quantity += Selectedquantity;
-//     }
-
-//        saveToStorage()
-
-
-
-// }
-
-
