e583c43ae3d8344ecfeb35f17f46e5f0f7da3942
Rafael <rafaelviana.contact@gmail.com>
2025-07-16 21:20:53 -0300

    using oop practice to improve the cart data

--- real-website-projects/AmazonProject/data/cart-oop.js
+++ real-website-projects/AmazonProject/data/cart-oop.js
@@ -1,103 +1,113 @@
 import { products, getProduct } from '../data/products.js';
 import { getDelivery } from '../data/delivery.js';
 
+const cart = {
+    cartItems: JSON.parse(localStorage.getItem('cart-oop')) || [],
 
-export let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    saveToStorage() {
+        localStorage.setItem('cart-oop', JSON.stringify(cart));
+    },
+
+    addToCart(productId, container) { // Method for add a product to the cart
+        const existingProduct = this.cartItems.find(product => product.id === productId);
+
+        const quantitySelector = container.querySelector('.js-quantity-selector');
+        const quantityValue = Number(quantitySelector.value);
+
+
+        // it was used for verify if the cart has the same product as you want to add to cart
+
+        if (!existingProduct) {
+            this.cartItems.push({
+                id: productId, // i just utilized only id and quantity attributes, because from the id i can search the all others attributes
+                quantity: quantityValue,
+                deliveryOptionId: '1'
+            });
+
+        }
+
+        else {
+            existingProduct.quantity += quantityValue;
+        }
+
+        this.saveToStorage();
+
+    },
+
+    removeFromCart(productId) { // method to remove a product of the cart
+        const newCart = this.cartItems.filter(product => product.id !== productId);
+        this.cartItems = newCart;
+        this.saveToStorage()
+    },
+
+    showCartQuantityText() { // this method shows the quantity of product has in the cart
+        const totalItems = this.cartItems.reduce((acumulator, currentProduct) => acumulator + currentProduct.quantity, 0);
+        const cartQuantity = document.querySelectorAll(`.js-cart-quantity`);
+        cartQuantity.forEach(quantityText => {
+            quantityText.textContent = totalItems;
+        })
+    },
+
+    updateCartQuantity(productId, actuallyQuantity) { // method for change the selected quantity of the product
+        const changedProduct = this.cartItems.find(product => product.id === productId);
+        changedProduct.quantity = actuallyQuantity;
+        this.saveToStorage()
+    },
+
+    changeDeliveryOption(productId, newDeliveryId) {
+        const cartProduct = this.cartItems.find(cartItem => cartItem.id === productId);
+        cartProduct.deliveryOptionId = newDeliveryId;
+
+        this.saveToStorage();
+
+    },
+
+    calculateCartTotals() {
+        const taxPercentage = 0.1;
+        const paymentSummary = {
+            totalItemsPrice: 0,
+            totalShipping: 0,
+            totalBeforeTax: 0,
+            totalTax: 0,
+            orderTotal: 0,
+        }
+
+        this.cartItems.forEach(cartProduct => {
+            const product = getProduct(cartProduct.id)
+            const deliveryProperties = getDelivery(cartProduct.deliveryOptionId);
+            paymentSummary.totalItemsPrice += product.priceCents * cartProduct.quantity;
+            paymentSummary.totalShipping += deliveryProperties.priceCents;
+
+        });
+        paymentSummary.totalBeforeTax = paymentSummary.totalItemsPrice + paymentSummary.totalShipping;
+        paymentSummary.totalTax = paymentSummary.totalBeforeTax * taxPercentage;
+        paymentSummary.orderTotal = paymentSummary.totalBeforeTax + paymentSummary.totalTax;
+
+        return paymentSummary;
+
+    },
+
+
+    resetCart() {
+        const resetedCart = []
+        localStorage.setItem('cart-oop', JSON.stringify(resetedCart));
+    },
+
+
+
+
+};
+
+
 
 // there is all cart methods. It doesn't include html changes
 
-const saveToStorage = () => {
-    localStorage.setItem('cart', JSON.stringify(cart));
-}
-
-export const addToCart = (productId, container) => { // Method for add a product to the cart
-    const existingProduct = cart.find(product => product.id === productId);
-
-    const quantitySelector = container.querySelector('.js-quantity-selector');
-    const quantityValue = Number(quantitySelector.value);
-
-
-    // it was used for verify if the cart has the same product as you want to add to cart
-
-    if (!existingProduct) {
-        cart.push({
-            id: productId, // i just utilized only id and quantity attributes, because from the id i can search the all others attributes
-            quantity: quantityValue,
-            deliveryOptionId: '1'
-        });
-
-    }
-
-    else {
-        existingProduct.quantity += quantityValue;
-    }
-
-    saveToStorage()
 
 
 
 
 
-};
 
-export const removeFromCart = (productId) => { // method to remove a product of the cart
-    const newCart = cart.filter(product => product.id !== productId);
-    cart = newCart;
-    saveToStorage()
-}
-
-export const showCartQuantityText = () => { // this method shows the quantity of product has in the cart
-    const totalItems = cart.reduce((acumulator, currentProduct) => acumulator + currentProduct.quantity, 0);
-    const cartQuantity = document.querySelectorAll(`.js-cart-quantity`);
-    cartQuantity.forEach(quantityText => {
-        quantityText.textContent = totalItems;
-    })
-};
-
-export const updateCartQuantity = (productId, actuallyQuantity) => { // method for change the selected quantity of the product
-    const changedProduct = cart.find(product => product.id === productId);
-    changedProduct.quantity = actuallyQuantity;
-    saveToStorage()
-};
-
-
-export const changeDeliveryOption = (productId, newDeliveryId) => {
-    const cartProduct = cart.find(cartItem => cartItem.id === productId);
-    cartProduct.deliveryOptionId = newDeliveryId;
-
-    saveToStorage();
-
-};
-
-export const calculateCartTotals = () => {
-    const taxPercentage = 0.1;
-    const paymentSummary = {
-        totalItemsPrice: 0,
-        totalShipping: 0,
-        totalBeforeTax: 0,
-        totalTax: 0,
-        orderTotal: 0,
-    }
-
-    cart.forEach(cartProduct => {
-        const product = getProduct(cartProduct.id)
-        const deliveryProperties = getDelivery(cartProduct.deliveryOptionId);
-        paymentSummary.totalItemsPrice += product.priceCents * cartProduct.quantity;
-        paymentSummary.totalShipping += deliveryProperties.priceCents;
-
-    });
-    paymentSummary.totalBeforeTax = paymentSummary.totalItemsPrice + paymentSummary.totalShipping;
-    paymentSummary.totalTax = paymentSummary.totalBeforeTax * taxPercentage;
-    paymentSummary.orderTotal = paymentSummary.totalBeforeTax + paymentSummary.totalTax;
-
-    return paymentSummary;
-
-}
-
-export const resetCart = () => {
-    const resetedCart = []
-    localStorage.setItem('cart', JSON.stringify(resetedCart));
-};
 
 
 
