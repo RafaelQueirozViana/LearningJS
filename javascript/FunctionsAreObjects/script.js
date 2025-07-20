3670f071c32a3216d91cab0adcff0481c4c76905
Rafael <rafaelviana.contact@gmail.com>
2025-07-20 20:24:34 -0300

    created the functions are objects folder

---
+++ javascript/FunctionsAreObjects/script.js
@@ -0,0 +1,10 @@
+// Functions are objects (or methods)
+
+// this is because by default the javascript has a set of functions that are used 
+// to program. These functions are methods inside an objects.
+
+// that's why we use . to access a property of the elements.
+
+// for example:
+
+document.addEventListener('click', () => { });
