18123b2832beb875a1384cd1b249942bb301cfbb
Rafael <rafaelviana.contact@gmail.com>
2025-07-20 20:20:48 -0300

    created the DOM folder

---
+++ javascript/DOM/dom.js
@@ -0,0 +1,16 @@
+// Most used DOM methods:
+
+document.getElementById('id');
+document.querySelector('.class');
+document.querySelectorAll('.class')
+document.addEventListener('click', () => { });
+
+// Example in real projects
+
+const button = document.querySelector('.class');
+
+button.addEventListener('click', () => { });
+
+
+
+
