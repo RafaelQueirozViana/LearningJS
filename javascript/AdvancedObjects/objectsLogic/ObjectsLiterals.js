163609b1446447641d28f421758a1be5b6b56402
Rafael <rafaelviana.contact@gmail.com>
2025-07-19 22:10:54 -0300

    object literals

---
+++ javascript/AdvancedObjects/objectsLogic/ObjectsLiterals.js
@@ -0,0 +1,13 @@
+// Object literal is the base form that we create a normal object
+
+// for example
+
+const circle = {
+    radius: 2,
+    message() {
+        console.log('hello')
+    },
+};
+
+// But it isn't the best way to create object and duplicate it if this object has at least 1 method
+
