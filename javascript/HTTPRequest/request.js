74ba70e3612da4c03c7db990bd904ab9f3655de8
Rafael <rafaelviana.contact@gmail.com>
2025-07-27 21:14:13 -0300

    commenting the html  request code

--- javascript/HTTPRequest/request.js
+++ javascript/HTTPRequest/request.js
@@ -1,12 +1,16 @@
-let message;
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://supersimplebackend.dev/not-supported');
-xhr.send();
 
+
+
+let message;
+const xhr = new XMLHttpRequest(); // it makes a request from the backend
+xhr.open('GET', 'https://supersimplebackend.dev/not-supported'); // Here you will tell of the the javascript what's the url from the backend
+xhr.send(); // here you will send the request to the backend
+
+// Remember: When is happening a request, it can take time to send the request and receive the response from the backend
+
+// Below is a code that add an eventListener load, that will be activate when de request gets the response from the backend
 xhr.addEventListener('load', () => {
     message = xhr.response;
     console.log(message)
-
-
 
 });
