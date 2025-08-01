4bbacedce8596296b0f1779a3fd31bf631035ea2
Rafael <rafaelviana.contact@gmail.com>
2025-08-01 16:02:39 -0300

    testing a login logic with asyncronous code

--- javascript/CallBacks/script.js
+++ javascript/CallBacks/script.js
@@ -1,23 +1,44 @@
 const loginUser = ({ email, age, onSucces, onError }) => {
-    const error = true;
+    const error = false;
 
     if (error) {
         return onError('the request was failed');
     }
     setTimeout(() => {
         console.log('user sended to database');
-        onSucces(email);
+        onSucces({ email });
 
     }, 1500);
 
 };
 
-const helloUser = (username) => {
-    console.log(`hello ${username}`);
+const printUser = (username) => {
+    console.log({ username });
 }
 
 const showError = (errorMessage) => {
     console.log(`error: ${errorMessage}`);
 }
 
-loginUser({ email: 'test@gmail.com', age: 12, onSucces: helloUser, onError: showError });
+const getVideos = (email, fun) => {
+    const a = setTimeout(() => {
+        fun(['video1', 'video2', 'video3']);
+    }, 1000);
+};
+
+
+
+//Home page 
+
+//Videos page
+
+loginUser({
+    email: 'test@gmail.com',
+    age: 12,
+    onSucces: ({ email }) => {
+        getVideos(email, (videos) => {
+            console.log(videos)
+        })
+    },
+    onError: showError
+});
