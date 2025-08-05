3dbd5ed0ce5e831b867572c27bf12958b4bf4bc2
Rafael <rafaelviana.contact@gmail.com>
2025-08-05 16:05:02 -0300

    creating my first promisse

--- javascript/Promisses/promisses.js
+++ javascript/Promisses/promisses.js
@@ -1,4 +1,4 @@
-const loginUser = ({ email, age, onSucces, onError }) => {
+const loginUser2 = ({ email, age, onSucces, onError }) => {
   // Simula um envio para o banco de dados com um delay de 1.5 segundos
   setTimeout(() => {
     const error = false; // Simulando se ocorreu um erro (hardcoded como false por enquanto)
@@ -12,3 +12,34 @@ const loginUser = ({ email, age, onSucces, onError }) => {
     onSucces({ email }); // Chama o callback de sucesso passando o email do usuário
   }, 1500);
 };
+
+
+
+const loginUser = (name, email) => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://supersimplebackend.dev/products')
+    xhr.send();
+
+    xhr.addEventListener('load', () => {
+      let data = 0;
+
+
+      if (!data) {
+        reject('error ocurred');
+      }
+
+      resolve(data);
+
+    });
+
+
+
+
+
+  });
+};
+
+loginUser()
+  .then((message) => console.log(message))
+  .catch((message) => console.log(message));
