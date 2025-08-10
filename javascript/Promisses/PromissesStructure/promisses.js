ebad55d11ba8366c2fd710124e144714470e0dfc
Rafael <rafaelviana.contact@gmail.com>
2025-08-10 14:51:43 -0300

    update promisses

--- javascript/Promisses/PromissesStructure/promisses.js
+++ javascript/Promisses/PromissesStructure/promisses.js
@@ -1,17 +1,3 @@
-const loginUser2 = ({ email, age, onSucces, onError }) => {
-  // Simula um envio para o banco de dados com um delay de 1.5 segundos
-  setTimeout(() => {
-    const error = false; // Simulando se ocorreu um erro (hardcoded como false por enquanto)
-
-    if (error) {
-      // Se houver erro, chama o callback de erro e interrompe a execução
-      return onError('the request was failed');
-    }
-
-    console.log('user sended to database'); // Log simulando envio ao banco
-    onSucces({ email }); // Chama o callback de sucesso passando o email do usuário
-  }, 1500);
-};
 
 
 
