3cf63667464c303c218688d605fbe567088b79f9
Rafael <rafaelviana.contact@gmail.com>
2025-08-27 15:05:04 -0300

    finished the challenge

--- javascript/LeetCodeChallenges/verifyMessageValid/index.js
+++ javascript/LeetCodeChallenges/verifyMessageValid/index.js
@@ -2,16 +2,49 @@
 const specialCharacters = ['()', '{}', '[]'];
 
 
+
+
 const verifyMessage = (message) => {
     const separatedWords = [...message];
-    console.log(separatedWords)
+
+    let haveEnd = true;
+
+    for (i = 0; i <= separatedWords.length - 1; i++) {
+        const currentLetter = separatedWords[i];
+
+        specialCharacters.forEach(specialCharacter => {
+            const charArray = [...specialCharacter];
+            if (currentLetter == specialCharacter[0]) {
+                if (haveEnd) {
+                    const nextLetter = separatedWords[i + 1]; // (
+                    if (nextLetter == specialCharacter[1]) {
+                        haveEnd = true;
+                    }
+
+                    else {
+                        haveEnd = false;
+                    }
+
+
+
+                }
+            }
+        });
+    }
+
+    return haveEnd;
+
+
+
+
+
+
+
 }
 
-verifyMessage('[()]')
+console.log(verifyMessage('([])')
+)
 
 
 
-specialCharacters.forEach(char => {
-    const
-})
 
