394ffc25a8e899d01103718adb0e5a7216b9d707
Rafael <rafaelviana.contact@gmail.com>
2025-12-05 14:58:22 -0300

    created animation section when scroll

--- origamid-course/active-tab-system/script.js
+++ origamid-course/active-tab-system/script.js
@@ -65,3 +65,25 @@ const initExternalLinks = () => {
 }
 
 initExternalLinks()
+
+
+const sections = document.querySelectorAll('.js-scroll');
+const sectionHalf = window.innerHeight * 0.6;
+
+function scrolAnimation() {
+    const activeClass = 'ativo'
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top - sectionHalf;
+
+        if (sectionTop <= 0) {
+            section.classList.add(activeClass);
+        }
+    });
+
+
+};
+
+scrolAnimation();
+
+window.addEventListener('scroll', scrolAnimation)
