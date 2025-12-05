0302a188b1cf95afc28a69fe74de4a8c9ac6d782
Rafael <rafaelviana.contact@gmail.com>
2025-12-04 21:39:53 -0300

    added animations to the tabclick menu

--- origamid-course/active-tab-system/script.js
+++ origamid-course/active-tab-system/script.js
@@ -1,17 +1,21 @@
 const tabClickMenu = document.querySelectorAll('.js-tabmenu li');
-
 const tabDescriptions = document.querySelectorAll('.js-tab-description aside');
 
 
-const openTabDesc = (index) => {
-    tabDescriptions.forEach(box => box.classList.remove('active'));
-    tabDescriptions[index].classList.add('active');
+const initTabMenu = () => {
+
+    if (tabClickMenu.length && tabDescriptions.length) {
+        const openTabDesc = (index) => {
+            tabDescriptions.forEach(box => box.classList.remove('active'));
+            tabDescriptions[index].classList.add('active');
+        }
+
+        tabClickMenu.forEach((btn, index) => {
+            btn.addEventListener('click', () => {
+                openTabDesc(index);
+            })
+        })
+    }
 }
 
-
-tabClickMenu.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-        openTabDesc(index);
-    })
-})
-
+initTabMenu()
