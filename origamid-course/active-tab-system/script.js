d52486e613d9b8babf034ff7b1e6a4e7fb89518e
Rafael <rafaelviana.contact@gmail.com>
2025-12-04 22:19:28 -0300

    created accordion list system

--- origamid-course/active-tab-system/script.js
+++ origamid-course/active-tab-system/script.js
@@ -1,13 +1,14 @@
-const tabClickMenu = document.querySelectorAll('.js-tabmenu li');
-const tabDescriptions = document.querySelectorAll('.js-tab-description aside');
-
-
 const initTabMenu = () => {
+
+    const activeClass = 'active'
+    const tabClickMenu = document.querySelectorAll('.js-tabmenu li');
+    const tabDescriptions = document.querySelectorAll('.js-tab-description aside');
+
 
     if (tabClickMenu.length && tabDescriptions.length) {
         const openTabDesc = (index) => {
-            tabDescriptions.forEach(box => box.classList.remove('active'));
-            tabDescriptions[index].classList.add('active');
+            tabDescriptions.forEach(box => box.classList.remove(activeClass));
+            tabDescriptions[index].classList.add(activeClass);
         }
 
         tabClickMenu.forEach((btn, index) => {
@@ -19,3 +20,18 @@ const initTabMenu = () => {
 }
 
 initTabMenu()
+
+const activeClass = 'active';
+const accordionButtons = document.querySelectorAll('.js-accordion h3')
+const accordionBox = document.querySelectorAll('.js-accordion li');
+
+
+function activeAccordion() {
+    const container = this.parentElement;
+    container.classList.toggle(activeClass);
+}
+
+
+accordionButtons.forEach(button => {
+    button.addEventListener('click', activeAccordion)
+});
