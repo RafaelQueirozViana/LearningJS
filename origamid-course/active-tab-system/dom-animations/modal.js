f7737a4ea01c98694da09e16e27ea4b38a505f16
Rafael <rafaelviana.contact@gmail.com>
2025-12-10 10:58:34 -0300

    finished the modal script

--- origamid-course/active-tab-system/dom-animations/modal.js
+++ origamid-course/active-tab-system/dom-animations/modal.js
@@ -2,6 +2,28 @@ export const initModal = () => {
     const openButton = document.querySelector('[data-modal="open-button"');
     const closeButton = document.querySelector('[data-modal="close-button"');
     const modalContainer = document.querySelector('[data-modal="container"');
+    const activeClass = 'active'
+
+    function openModal(event) {
+        event.preventDefault();
+
+        if (modalContainer.classList.contains(activeClass)) {
+            closeModal();;
+        }
+
+        else {
+            modalContainer.classList.add(activeClass);
+
+        }
+    }
+
+    function closeModal() {
+        modalContainer.classList.remove(activeClass);
+
+    }
+
+    openButton.addEventListener('click', openModal);
+    closeButton.addEventListener('click', closeModal);
 
 }
 
