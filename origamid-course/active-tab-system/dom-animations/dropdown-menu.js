13afa8fffed44251fe242272ceacde72d4f419b4
Rafael <rafaelviana.contact@gmail.com>
2026-01-03 21:22:31 -0300

    learning event bubble

--- origamid-course/active-tab-system/dom-animations/dropdown-menu.js
+++ origamid-course/active-tab-system/dom-animations/dropdown-menu.js
@@ -1,4 +1,38 @@
 export const initDropdown = () => {
+
     const dropdownButton = document.querySelector('[data-dropdown]');
-    console.log(dropdownButton)
+
+    ['click', 'touchstart'].forEach(userEvent => {
+        dropdownButton.addEventListener(userEvent, handleClick);
+    });
+
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add('active');
+        outsideClick(this, () => {
+            this.classList.remove('active');
+        });
+
+    };
+
+    function outsideClick(element, callback) {
+        const html = document.documentElement;
+        html.addEventListener('click', handleOutsideClick);
+
+        function handleOutsideClick(event) {
+            if (!element.contains(event.target)) {
+                callback();
+
+            }
+        }
+
+    };
+
+
+
+
+
+
+
+
 }
