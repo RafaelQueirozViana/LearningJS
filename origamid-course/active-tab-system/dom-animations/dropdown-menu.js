0b2e16e30006cc1b5d92b80890bc568e7a26a2a0
Rafael <rafaelviana.contact@gmail.com>
2026-01-04 10:47:01 -0300

    created outside function to dropdown menu[

--- origamid-course/active-tab-system/dom-animations/dropdown-menu.js
+++ origamid-course/active-tab-system/dom-animations/dropdown-menu.js
@@ -2,37 +2,36 @@ export const initDropdown = () => {
 
     const dropdownButton = document.querySelector('[data-dropdown]');
 
-    ['click', 'touchstart'].forEach(userEvent => {
-        dropdownButton.addEventListener(userEvent, handleClick);
-    });
+    ['click', 'touchstart'].forEach(userEvent => dropdownButton.addEventListener(userEvent, handleClick))
 
     function handleClick(event) {
-        event.preventDefault();
         this.classList.add('active');
-        outsideClick(this, () => {
+        clickOutside(this, () => {
             this.classList.remove('active');
         });
 
-    };
+    }
 
-    function outsideClick(element, callback) {
+    function clickOutside(element, callback) {
         const html = document.documentElement;
-        html.addEventListener('click', handleOutsideClick);
+        const outside = 'data-outside'
 
-        function handleOutsideClick(event) {
-            if (!element.contains(event.target)) {
-                callback();
-
-            }
+        if (!element.hasAttribute(outside)) {
+            console.log('has outside');
+            html.addEventListener('click', handleClickOutside);
+            element.setAttribute(outside, '');
         }
 
+        function handleClickOutside(event) {
+            if (!element.contains(event.target)) {
+                callback();
+                html.removeEventListener('click', handleClickOutside);
+                element.removeAttribute(outside);
+            }
+        };
     };
 
-
-
-
-
-
-
-
 }
+
+
+
